5913f4aa9f76def54fe48613947971e779d38eee
AJ Vrieland <zer0relm@gmail.com>
2022-03-09 16:27:58 -0500

    Added graph switching

--- src/app/graph-cards/graph-cards.component.ts
+++ src/app/graph-cards/graph-cards.component.ts
@@ -15,25 +15,27 @@ import {FormControl} from '@angular/forms';
 export class GraphCardsComponent implements OnInit {
 
   // data_1: dataPoint = {time: "", co2: 0, humidity: 0, temperature: -273};
-  mergeOptions_1 = {};
-  mergeOptions_2 = {};
-  mergeOptions_3 = {};
+  mergeOptions_1_false = {};
+  mergeOptions_1_true = {};
+  mergeOptions_2_false = {};
+  mergeOptions_2_true = {};
+  mergeOptions_3_false = {};
+  mergeOptions_3_true = {};
   isLoading = false;
 
   // initial_data = [this.data_1.co2, this.data_1.humidity, this.data_1.temperature];
-  // new_data: number[] = [];
-  new_data: dataPoint = {time: [], co2: [], humidity: [], temperature: []}
+  // pi1_data: number[] = [];
+  pi1_data: dataPoint = {time: [], co2: [], humidity: [], temperature: []}
 
   // item: AngularFirestoreCollection<dataPoint>
 
   ngOnInit(): void {
   }
-  show_options: Boolean = false
-  change_show_options() {
-    this.show_options = !this.show_options
-    console.log(this.show_options)
-    console.log(this.Readings_1.value)
-  }
+  show_options: boolean = false
+  // change_show_options() {
+  //   this.show_options = !this.show_options
+
+  // }
   Readings_1 = new FormControl();
   Readings_2 = new FormControl();
   options_list: string[] = ["CO2", "Humidity", "Temperature", "Time"]
@@ -53,7 +55,7 @@ export class GraphCardsComponent implements OnInit {
       type: 'category',
       name: "Time",
       boundaryGap: false
-      // data: this.new_data.time,
+      // data: this.pi1_data.time,
     },
     tooltip: {},
     yAxis: {
@@ -80,15 +82,25 @@ export class GraphCardsComponent implements OnInit {
       }  
     )
     if(this.new_length > this.old_length){
-      // console.log(this.new_length)
-      // console.log(this.old_length)
       for(let i = this.old_length; i < this.new_length; i++){
-        this.new_data.co2.push(this.db_data[i].co2)
-        this.new_data.humidity.push(this.db_data[i].humidity)
-        this.new_data.temperature.push(this.db_data[i].temp)
-        this.new_data.time.push(this.db_data[i].time)
+        this.pi1_data.co2.push(this.db_data[i].co2)
+        this.pi1_data.humidity.push(this.db_data[i].humidity)
+        this.pi1_data.temperature.push(this.db_data[i].temp)
+        this.pi1_data.time.push(this.db_data[i].time)
+
         clearInterval(this.interval)
       }
+      // if(this.pi1_data.time.length >= 48){
+      //   let diff = this.pi1_data.time.length - 48
+      //   for(let i = 0; i < diff; i++){
+      //     this.pi1_data.co2.shift()
+      //     this.pi1_data.humidity.shift()
+      //     this.pi1_data.temperature.shift()
+      //     this.pi1_data.time.shift()
+      //   }
+      // }
+
+
       this.old_length = this.new_length
     }
   }
@@ -97,10 +109,10 @@ export class GraphCardsComponent implements OnInit {
     
     this.grab_data()
 
-    this.mergeOptions_1 = {
+    this.mergeOptions_1_true = {
       xAxis: [
         {
-          data: this.new_data.time,
+          data: this.pi1_data.time.slice(this.new_length - 48, this.new_length),
         }
       ],
       yAxis: [
@@ -110,16 +122,36 @@ export class GraphCardsComponent implements OnInit {
       ],
       series: [
         {
-          name: "co2 vs. time",
-          data: this.new_data.co2,
+          name: "CO2 vs. Time",
+          data: this.pi1_data.co2.slice(this.new_length - 48, this.new_length),
           type: "line",
         }
       ]
     }
-    this.mergeOptions_2 = {
+    this.mergeOptions_1_false = {
       xAxis: [
         {
-          data: this.new_data.time,
+          data: this.pi1_data.time,
+        }
+      ],
+      yAxis: [
+        {
+          name: "ppm"
+        }
+      ],
+      series: [
+        {
+          name: "CO2 vs. Time",
+          data: this.pi1_data.co2,
+          type: "line",
+        }
+      ]
+    }
+
+    this.mergeOptions_2_true = {
+      xAxis: [
+        {
+          data: this.pi1_data.time.slice(this.new_length - 48, this.new_length),
         }
       ],
       yAxis: [
@@ -129,16 +161,37 @@ export class GraphCardsComponent implements OnInit {
       ],
       series: [
         {
-          name: "Test_data",
-          data: this.new_data.temperature,
+          name: "Temperature vs. Time",
+          data: this.pi1_data.temperature.slice(this.new_length - 48, this.new_length),
           type: "line"
         }
       ]
     }
-    this.mergeOptions_3 = {
+    this.mergeOptions_2_false = {
       xAxis: [
         {
-          data: this.new_data.time,
+          data: this.pi1_data.time,
+        }
+      ],
+      yAxis: [
+        {
+          name: "temp C"
+        }
+      ],
+      series: [
+        {
+          name: "Temperature vs. Time",
+          data: this.pi1_data.temperature,
+          type: "line"
+        }
+      ]
+    }
+
+
+    this.mergeOptions_3_true = {
+      xAxis: [
+        {
+          data: this.pi1_data.time.slice(this.new_length - 48, this.new_length),
         }
       ],
       yAxis: [
@@ -148,8 +201,27 @@ export class GraphCardsComponent implements OnInit {
       ],
       series: [
         {
-          name: "Test_data",
-          data: this.new_data.humidity,
+          name: "Humidity vs. Time",
+          data: this.pi1_data.humidity.slice(this.new_length - 48, this.new_length),
+          type: "line"
+        }
+      ]
+    }
+    this.mergeOptions_3_false = {
+      xAxis: [
+        {
+          data: this.pi1_data.time,
+        }
+      ],
+      yAxis: [
+        {
+          name: "%"
+        }
+      ],
+      series: [
+        {
+          name: "Humidity vs. Time",
+          data: this.pi1_data.humidity,
           type: "line"
         }
       ]
